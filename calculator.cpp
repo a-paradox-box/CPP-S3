15a8795993c08cdd5697820ac58ce3498469ce34
alenpaul2001 <alenpaul2001@gmail.com>
2021-01-20 10:51:00 +0530

    added somethings

---
+++ calculator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(){
+	int a, b;
+	int result = 0;
+	char op;
+	std::cout<<"Enter the first number: ";
+	std::cin>>a;
+	std::cout<<"choose the operator (+, -, /, *): ";
+	std::cin>>op;
+	std::cout<<"Enter the second number: ";
+	std::cin>>b;
+	switch (op){
+	case '+':
+		result = a + b;
+		break;
+	case '-':
+		result = a - b;
+		break;
+	case '*':
+		result = a * b;
+		break;
+	case '/':
+		result = a / b;
+		break;
+	}
+	std::cout<<"Result: "<<result<<std::endl;
+}
---
+++ reverse.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main(){
+    int n=0, reversed=0, original, remainder;
+    std::cout << "Enter a number : ";
+    std::cin >> n;
+    original = n;
+    while (n != 0) {
+            remainder = n % 10;
+            reversed = reversed * 10 + remainder;
+            n /= 10;
+        }
+    std::cout<<"Reverse: "<<reversed<<std::endl;
+}
